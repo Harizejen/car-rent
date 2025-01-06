63ef6acecaa859ac7da6320e05eae349f350fba4
Itqnazs <itqnazs248@gmail.com>
2025-01-06 21:49:36 +0800

    Add files via upload
    Database

---
+++ db/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost:1521/xe"; // Replace with your Oracle service name
+$username = "CARRENTAL"; // Oracle username
+$password = "123456"; // Oracle password
+
+// Establish connection
+$conn = oci_connect($username, $password, $host);
+
+if (!$conn) {
+    $e = oci_error();
+    die("Connection failed: " . $e['message']);
+}
+?>
