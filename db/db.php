c006420359e273e5b5dbf2e28e6a6adfd3172eec
Itqnazs <itqnazs248@gmail.com>
2025-01-26 04:21:03 +0800

    db

--- db/db.php
+++ db/db.php
@@ -2,7 +2,7 @@
 
 $host = "localhost:1521/xe"; // Replace with your Oracle service name
 $username = "CARRENTAL"; // Oracle username
-$password = "SYSTEM"; // Oracle password
+$password = "123456"; // Oracle password
 
 // Establish connection
 $conn = oci_connect($username, $password, $host);
